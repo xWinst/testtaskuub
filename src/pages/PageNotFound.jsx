a2b0bcf980cf4fbee4643ec9093575a71a29bcb9
Oleg <hellreebok@gmail.com>
2023-02-08 20:21:20 +0200

    add page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import image from 'images/404PageNotFound.svg';
+
 const PageNotFound = () => {
-  return <>404</>;
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate('/', { replace: true });
+  };
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <h2 className="mt-2">404 Page Not Found</h2>
+      <img src={image} alt="Page Not Found" style={{ maxWidth: 800 }} />
+      <p className="h3">Упс! Сторінка, яку ви шукаєте, не існує :(</p>
+      <button className="btn btn-primary mt-4" type="button" onClick={onClick}>
+        На головну
+      </button>
+    </div>
+  );
 };
 
 export default PageNotFound;
